be076532c7002955a8086d7fe2c037dc6cb28bab
Taupilataup <57759901+Taupilataup@users.noreply.github.com>
2020-02-03 14:14:20 +0100

    Parser avec titre

--- parser.jl
+++ parser.jl
@@ -1,11 +1,12 @@
-function parser_import(file_path, separator=',')
+function parser_import(file_path, hasheader=true, separator=',')
+    i = 1 + hasheader
     open(file_path) do f
         content = readlines(f)
         parser = []
-        for index in range(2, stop = length(content))
+        for index in range(i, stop = length(content))
             append!(parser, [split(content[index],separator)])
         end
         return parser
     end
 end
-parser_import("test.csv")
+parser_import("Itineraires.csv")
