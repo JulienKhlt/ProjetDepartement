9f542a2b4c8efad4888647f743ca24a9badf44c3
AdrienB4000 <60354760+AdrienB4000@users.noreply.github.com>
2020-02-07 03:23:48 +0100

    calcul_gain_quasi_final

--- calcul_gain.jl
+++ calcul_gain.jl
@@ -4,13 +4,12 @@ Itineraires=parser_import("Itineraire_escales_prix.csv")
 Demande=parser_import("Demandes2.csv")
 Capacites=parser_import("Capacites2.csv")
 
-nbvols=round(Int,length(Demande)/2)
-itparvol=3
+nbvols=length(Itineraires)
 leg=length(Capacites)
-#En comptant le choix de ne pas acheter
+nbpers=length(Demande)
 
-prix=zeros(nbvols,itparvol)
-proba=ones(nbvols,itparvol)
+
+proba=ones(nbvols)
 
 nb_sommet=4
 
@@ -26,20 +25,12 @@ function ver_tuple_liste(l,a,b)
 end
 
 function lecture_itin()
-    #Pour chaque leg j, itin_leg[i][j] vaut 1 si l'itinéraire i utlise le leg j
-    n=length(Itineraires)
-    itin_leg=[]
-
-    for i=1:n
-        l=[]
-        for j=2:4
-            if parse(Int,Itineraires[i][j])!=0
-                append!(l,parse(Int,Itineraires[i][j]))
-            end
-        end
-        append!(itin_leg,[l])
+    #On remplit ici le tableau des prix ou prix[i] est le prix de l'itineraire i
+    prix=zeros(nbvols)
+    for i=1:nbvols
+        prix[i]=parse(Int,Itineraires[i][7])
     end
-    #Une fois ce tableau obtenu, on construit leg_itin qui pour chaque leg (i,j), contient
+    #Après cela, on construit leg_itin qui pour chaque leg (i,j), contient
     #tous les itinéraires qui contiennent ce leg
     leg_itin=[]
     for i in 1:leg
@@ -47,13 +38,13 @@ function lecture_itin()
         dep=Capacites[i][2]
         arr=Capacites[i][3]
         for j in 1:nbvols
-            if ver_tuple_liste(Itineraires[2*j],dep,arr)
+            if ver_tuple_liste(Itineraires[j],dep,arr)
                 append!(l,j)
             end
         end
         append!(leg_itin,[l])
     end
-    return leg_itin
+    return (prix,leg_itin)
 end
 
 function lecture_capa()
@@ -67,33 +58,44 @@ function lecture_capa()
 end
 
 function lecture_demande()
-    demande_pers=zeros(nbvols,itparvol)
+    demande_pers=zeros(nbvols)
     #Initialisation de demande_pers
-    #demande_pers[i][j] représente le nombre de personnes prenant l'itinéraire j du vol i
-    for i = 1:nbvols
-        for j = 1:itparvol
-            demande_pers[i,j]=proba[i,j]*(parse(Int,Demande[2*i][4])+parse(Int,Demande[2*i-1][4]))
-            #Demande[i][4] est la demande totale du vol i
-            #parse sert à convertir en entier
-        end
+    #demande_pers[i] représente le nombre de personnes prenant l'itinéraire i
+    for i = 1:nbpers
+        demande_pers[parse(Int,Demande[i][6])]+=""Proba que la personne prenne l'itin eco""*parse(Int,Demande[i][4])
+        demande_pers[parse(Int,Demande[i][7])]+=""Proba que la personne prenne l'itin bus""*parse(Int,Demande[i][4])
     end
     return demande_pers
 end
 
 function gestion_cap()
+    #Cette fonction vérifie que sur chaque leg, la capacité n'est pas dépassée
+    #Si elle est dépassée, cette fonction enlève des personnes
     cap=lecture_capa()
     demande_pers=lecture_demande()
-    L=sum(demande_pers[:,2:itparvol],dims=2)
-    #Cela donne un tableau où le i eme nombre est le
-    #nombre de personnes empruntant l'itinéraire i
-    return L
+    (prix,leg_itin)=lecture_itin()
+    for i = 1:leg
+        #dem représente la demande réelle du leg en question
+        dem=0
+        for j in leg_itin[i]
+            dem+=demande_pers[j]
+        end
+        if dem>cap[i]
+            for j in leg_itin[i]
+                #Avec cette ligne on ramène la demande à la capacité du leg
+                demande_pers[j]=demande_pers[j]*cap[i]/dem
+            end
+        end
+    end
+    return demande_pers
 end
 
 
 function gain()
-    demande_pers=lecture_demande()
-    #Initialisation de demande_pers
-    #demande_pers[i][j] représente le nombre de personnes prenant l'itinéraire j du vol i
+    demande_pers=gestion_cap()
+    prix=lecture_itin()[1]
+    #demande_pers[i] représente le nombre de personnes prenant l'itinéraire i
+    #et prix[i] le prix de cet itinéraire
     gain=sum(prix.*demande_pers)
     return gain
 end
