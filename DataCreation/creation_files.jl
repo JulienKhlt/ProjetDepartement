5100abdf1794341a22f25714426298902ddff465
Seb <sebastien.deschamps@enpc.fr>
2020-03-02 14:43:14 +0100

    Fonctions de creation de base de donnee

---
+++ DataCreation/creation_files.jl
@@ -0,0 +1,187 @@
+using Random
+
+# Structured used
+struct Airports
+    id::Int64
+    period::Int64
+end
+
+struct Flights
+    id::Int64
+    airport_dep::Int64
+    airport_arr::Int64
+    capacity::Int64
+end
+
+struct OnD
+    id::Int64
+    origin::Int64
+    destination::Int64
+    demand_buisiness::Int64
+    demand_family::Int64
+end
+
+struct Itinerary
+    id::Int64
+    departure::Int64
+    arrival::Int64
+    OnD::Int64
+    list_visited_airports::Array{Int64, 1}
+    alpha::Float64
+end
+
+struct CrossData
+    flight_to_itin::Array{Array{Int64, 1}, 1}
+    itin_to_flight::Array{Array{Int64, 1}, 1}
+    itin_to_OnD::Array{Int64, 1}
+    Ond_to_itin::Array{Array{Int64, 1}, 1}
+end
+
+# Creation of the flight graph
+function create_graph(nb_node_min, nb_node_max, nb_edge_1_min, nb_edge_1_max, nb_edge_2_min, nb_edge_2_max, capa_poss, nb_per)
+    rng = MersenneTwister(1234)
+    list_airports = Array{Airports, 1}()
+    list_flights = Array{Flights, 1}()
+    airport_to_flight_in = Array{Array{Int64, 1}, 1}()
+    airport_to_flight_out = Array{Array{Int64, 1}, 1}()
+    list_per_to_node = Array{Array{Int64, 1}, 1}()
+    per_to_nb_node = Array{Int64, 1}()
+    airport_id = 0
+    flight_id = 0
+    for per in 1:nb_per
+        nb_nodes_per = rand(nb_node_min:nb_node_max)
+        push!(per_to_nb_node, nb_nodes_per)
+        push!(list_per_to_node, [])
+        for i in 1:nb_nodes_per
+            airport_id += 1
+            push!(list_per_to_node[per], airport_id)
+            push!(list_airports, Airports(airport_id, per))
+            push!(airport_to_flight_in, [])
+            push!(airport_to_flight_out, [])
+        end
+    end
+    for per in 1:(nb_per-2)
+        for departure in list_per_to_node[per]
+            nb_edges_1 = min(per_to_nb_node[per+1], rand(nb_edge_1_min:nb_edge_1_max))
+            nb_edges_2 = min(per_to_nb_node[per+2], rand(nb_edge_2_min:nb_edge_2_max))
+            list_selected_1 = randperm(Random.seed!(rng), length(list_per_to_node[per+1]))[1:nb_edges_1]
+            list_selected_2 = randperm(Random.seed!(rng), length(list_per_to_node[per+2]))[1:nb_edges_2]
+            for k in list_selected_1
+                arrival = list_per_to_node[per+1][k]
+                flight_id += 1
+                capa = rand(capa_poss)
+                push!(list_flights, Flights(flight_id, departure, arrival, capa))
+                push!(airport_to_flight_in[arrival], flight_id)
+                push!(airport_to_flight_out[departure], flight_id)
+            end
+            for k in list_selected_2
+                arrival = list_per_to_node[per+2][k]
+                flight_id += 1
+                capa = rand(capa_poss)
+                push!(list_flights, Flights(flight_id, departure, arrival, capa))
+                push!(airport_to_flight_in[arrival], flight_id)
+                push!(airport_to_flight_out[departure], flight_id)
+            end
+        end
+    end
+
+    return list_airports, list_flights, airport_to_flight_in, airport_to_flight_out, list_per_to_node
+end
+
+# Function that creates an itinerary
+function add_itinerary(list_it, i_t_OD, i_t_f, OD_t_i, id_it, id_od, d_node, a_node, list_nodes, list_f, alph)
+    i_to_add = Itinerary(id_it, d_node, a_node, id_od, list_nodes, alph)
+    push!(list_it, i_to_add)
+    push!(i_t_OD, id_od)
+    push!(i_t_f, list_f)
+    push!(OD_t_i[id_od], id_it)
+end
+
+# Creation pf the Onds and the itineraries
+function create_itineraries(per_t_n, airp, list_demand_busi, list_demand_fam, edg_out, list_flights)
+    list_OnD = Array{OnD, 1}()
+    list_itineraries = Array{Itinerary, 1}()
+    id_OnD = 0
+    id_itin = 0
+
+    OD_to_id = Dict{Tuple{Int64, Int64}, Int64}()
+    flight_to_itin = Array{Array{Int64, 1}, 1}()
+    for i in list_flights
+        push!(flight_to_itin, [])
+    end
+    itin_to_flight = Array{Array{Int64, 1}, 1}()
+    itin_to_OnD = Array{Int64, 1}()
+    Ond_to_itin = Array{Array{Int64, 1}, 1}()
+
+    # Creation of the OnDs
+    for k in 1:4
+        for (i, period) in enumerate(per_t_n[1:end-k])
+            for dep_node in period
+                for arr_node in per_t_n[i+k]
+                    id_OnD += 1
+                    demand_b = rand(list_demand_busi)
+                    demand_f = rand(list_demand_fam)
+                    push!(list_OnD, OnD(id_OnD, dep_node, arr_node, demand_b, demand_f))
+                    push!(Ond_to_itin, [])
+                    OD_to_id[(dep_node, arr_node)] = id_OnD
+                    # Itinerary no buy
+                    id_itin += 1
+                    alpha_no_buy = rand()
+                    add_itinerary(list_itineraries, itin_to_OnD, itin_to_flight, Ond_to_itin, id_itin, id_OnD, dep_node, arr_node, [], [], alpha_no_buy)
+                end
+            end
+        end
+    end
+
+    # Creation of itineraries
+    for period in per_t_n[1:end-2]
+        for dep_node in period
+            for flight_1 in list_flights[edg_out[dep_node]]
+                aer_1 = flight_1.airport_arr
+                used_OnD_1 = OD_to_id[(dep_node, aer_1)]
+                visited_aer_1 = [dep_node, aer_1]
+                id_itin += 1
+                alpha_fam = rand()
+                add_itinerary(list_itineraries, itin_to_OnD, itin_to_flight, Ond_to_itin, id_itin, used_OnD_1, dep_node, aer_1, visited_aer_1, [flight_1.id], alpha_fam)
+                push!(flight_to_itin[flight_1.id], id_itin)
+                id_itin += 1
+                alpha_busi = rand() + 1
+                add_itinerary(list_itineraries, itin_to_OnD, itin_to_flight, Ond_to_itin, id_itin, used_OnD_1, dep_node, aer_1, visited_aer_1, [flight_1.id], alpha_busi)
+                push!(flight_to_itin[flight_1.id], id_itin)
+                for id_fl_2 in edg_out[aer_1]
+                    flight_2 = list_flights[id_fl_2]
+                    aer_2 = flight_2.airport_arr
+                    used_OnD_2 = OD_to_id[(dep_node, aer_2)]
+                    visited_aer_2 = [dep_node, aer_1, aer_2]
+                    id_itin += 1
+                    alpha_fam = rand()
+                    add_itinerary(list_itineraries, itin_to_OnD, itin_to_flight, Ond_to_itin, id_itin, used_OnD_2, dep_node, aer_2, visited_aer_2, [flight_1.id, flight_2.id], alpha_fam)
+                    push!(flight_to_itin[flight_2.id], id_itin)
+                    id_itin += 1
+                    alpha_busi = rand() + 1
+                    add_itinerary(list_itineraries, itin_to_OnD, itin_to_flight, Ond_to_itin, id_itin, used_OnD_2, dep_node, aer_2, visited_aer_2, [flight_1.id, flight_2.id], alpha_busi)
+                    push!(flight_to_itin[flight_2.id], id_itin)
+                end
+            end
+        end
+    end
+
+    # Itinerariess of the last period of the graph
+    for dep_node in per_t_n[end-1]
+        for flight in list_flights[edg_out[dep_node]]
+            aer_1 = flight.airport_arr
+            used_OnD = OD_to_id[(dep_node, aer_1)]
+            visited_aer = [dep_node, aer_1]
+            id_itin += 1
+            alpha_fam = rand()
+            add_itinerary(list_itineraries, itin_to_OnD, itin_to_flight, Ond_to_itin, id_itin, used_OnD, dep_node, aer_1, visited_aer, [flight.id], alpha_fam)
+            push!(flight_to_itin[flight.id], id_itin)
+            id_itin += 1
+            alpha_busi = rand() + 1
+            add_itinerary(list_itineraries, itin_to_OnD, itin_to_flight, Ond_to_itin, id_itin, used_OnD, dep_node, aer_1, visited_aer, [flight.id], alpha_busi)
+            push!(flight_to_itin[flight.id], id_itin)
+        end
+    end
+    links = CrossData(flight_to_itin, itin_to_flight, itin_to_OnD, Ond_to_itin)
+    return list_OnD, list_itineraries, links
+end
---
+++ DataCreation/csv_writer.jl
@@ -0,0 +1,83 @@
+# Generic function that create a csv
+function write_new_file(filename, header, lines)
+    open(filename, "w") do file
+        if header != ""
+            header_string = string(header, "\n")
+        else
+            header_string = string(header)
+        end
+        write(file, header_string)
+        for line in lines
+            line_written = string(line, "\n")
+            write(file, line_written)
+        end
+    end
+end
+
+# function that create the flight file
+function write_flight_file(folder_name, list_flights, links)
+    header = "id;depart;arrivee;capacite du vol;list_itineraires"
+    list_lines = []
+
+    for flight in list_flights
+        line = string(flight.id, ";", flight.airport_dep, ";", flight.airport_arr, ";", flight.capacity, ";", links.flight_to_itin[flight.id])
+        push!(list_lines, line)
+    end
+
+    write_new_file(string(folder_name, "/flight.csv"), header, list_lines)
+end
+
+# function that create the itinerary file
+function write_itinerary_file(folder_name, list_itineraries, links)
+    header = "id;depart;arrivee;OnD;list_airports;alpha;list_flights"
+    list_lines = []
+
+    for itinerary in list_itineraries
+        if itinerary.list_visited_airports == []
+                line = string(itinerary.id, ";", itinerary.departure, ";", itinerary.arrival, ";", itinerary.OnD, ";[];", itinerary.alpha, ";[]")
+                push!(list_lines, line)
+        else
+            line = string(itinerary.id, ";", itinerary.departure, ";", itinerary.arrival, ";", itinerary.OnD, ";", itinerary.list_visited_airports, ";", itinerary.alpha, ";", links.itin_to_flight[itinerary.id])
+            push!(list_lines, line)
+        end
+    end
+
+    write_new_file(string(folder_name, "/itineraries.csv"), header, list_lines)
+end
+
+# function that create the OnD file
+function write_OnD_file(folder_name, list_OnDs, links)
+    header = "id;depart;arrivee;demand_business;demand_family;list_itineraires"
+    list_lines = []
+
+    for OnD in list_OnDs
+        line = string(OnD.id, ";", OnD.origin, ";", OnD.destination, ";", OnD.demand_buisiness, ";", OnD.demand_family, ";", links.Ond_to_itin[OnD.id])
+        push!(list_lines, line)
+    end
+
+    write_new_file(string(folder_name, "/OnD.csv"), header, list_lines)
+end
+
+# function that build a folder
+function create_folder(folder_name)
+    if folder_name[end] == '/'
+        folder_name = folder_name[1:end-1]
+    end
+    final_name = folder_name
+    i = 0
+    while ispath(final_name)
+        final_name = string(folder_name, string(i))
+        i += 1
+    end
+    final_name = string(final_name, '/')
+    mkdir(final_name)
+    return final_name
+end
+
+# Function that build a whole database
+function create_database(name_folder, list_flights, list_itineraries, list_OnDs, links)
+    name_folder = create_folder(string("Data/", name_folder, "/"))
+    write_flight_file(name_folder, list_flights, links)
+    write_itinerary_file(name_folder, list_itineraries, links)
+    write_OnD_file(name_folder, list_OnDs, links)
+end
