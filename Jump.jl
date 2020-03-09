024167bbb363d0ced774bf59266ddc1d212cc7eb
artben98 <60355644+artben98@users.noreply.github.com>
2020-03-09 16:53:16 +0100

    Add files via upload

--- Jump.jl
+++ Jump.jl
@@ -3,35 +3,44 @@ include("tools.jl")
 include("proba.jl")
 include("calcul_gain.jl")
 
+
+
+
+
 Capa = lecture_capa(parser_import("DataCreation/Data/little0/flight.csv"))
 
 
 OnD = parser_chiffre(parser_import("DataCreation/Data/little0/OnD.csv"), [6])
 flight = parser_chiffre(parser_import("DataCreation/Data/little0/flight.csv"), [5])
 itineraries = parser_chiffre(parser_import("DataCreation/Data/little0/itineraries.csv"), [5,7], [6])
-
+alpha = calculer_alpha(parser_import("DataCreation/Data/little0/itineraries.csv"))
 
 using JuMP
-using Clp
+using GLPK
+
+
 
 function carotte(v)
 
-    m = Model(solver = ClpSolver)
+    ##m = Model(GLPK.Optimizer)
+    m = Model(with_optimizer(GLPK.Optimizer))
 
-    @variable(m,q[i=1:length(itneraries)] >=0)
-    @constraint(m,CapaciteLeg[i=1:length(Capa)], sum(q[j] for j in itineraries[i][7]) <= Capa[i] )
-    @constraint(m,DemandeOD[i=1:length(OnD)], sum(q[j] for j in OnD[i][6])= OnD[i][4]+OnD[i][5])
+
+    @variable(m,q[i=0:length(itineraries)] >=0)
+    @constraint(m,baba,q[0]==0)
+    @constraint(m,CapaciteLeg[i=1:length(Capa)], sum(q[j] for j in itineraries[i][6]) <= Capa[i] )
+    @constraint(m,DemandeOD[i=1:length(OnD)], sum(q[j] for j in OnD[i][6]) == OnD[i][4]+OnD[i][5])
 
     for j=1:length(OnD)
-         @constraint(m,Attractivite[i in OnD[j][6]], q[i]/itineraries[i][6] <= q[OnD[j][6]]/itineraries[OnD[j][6]][6] )
+         @constraint(m,[i=1:length(OnD[j][6])], q[OnD[j][6][i]]/alpha[OnD[j][6][i]] <= q[OnD[j][6][1]]/alpha[OnD[j][6][1]] )
     end
 
 
-    @objective(m, Max, sum(q[i]*v[i] for i=1:length(itneraries)))
+    @objective(m, Max, sum(q[i]*v[i] for i=1:length(itineraries)))
 
 
-    status = solve(m)
+    optimize!(m)
 
-    println("Objective value: ", getobjectivevalue(m))
-    println("repartition = ", getvalue(q))
+    println("Objective value: ", JuMP.objective_value(m))
+    println("repartition = ", JuMP.value.(q))
 end
